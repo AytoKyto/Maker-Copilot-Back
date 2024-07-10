bc8d325b7609a6ee62ff698a5c60f673b8217ed9
AytoDev <mathis@lanageuse.com>
2024-07-10 17:04:29 +0200

    add

---
+++ src/Entity/ViewBenefitMonth.php
@@ -0,0 +1,50 @@
+<?php
+
+// src/Entity/ViewBenefitMonth.php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Metadata\GetCollection;
+
+#[ORM\Entity(readOnly: true)]
+#[ORM\Table(name: "view_benefit_month")]
+#[ApiResource(
+    paginationEnabled: false,
+    operations: [
+        new GetCollection(),
+    ]
+)] 
+#[ApiFilter(RangeFilter::class, properties: ['month', 'years'])]
+#[ApiFilter(OrderFilter::class, properties: ['date_full' => 'DESC'])]
+
+
+class ViewBenefitMonth
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "integer")]
+    public int $user_id;
+
+    #[ORM\Column(type: "float")]
+    public float $benefit_value;
+
+    #[ORM\Column(type: "float")]
+    public float $price_value;
+
+    #[ORM\Column(type: "float")]
+    public float $benefit_pourcent;
+
+
+    #[ORM\Column(type: "string", length: 4)]
+    public string $years;
+
+    #[ORM\Column(type: "string", length: 2)]
+    public string $month;
+
+    #[ORM\Column(type: "string", length: 7)]
+    public string $date_full;
+}
